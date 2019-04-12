046029b04817bf1791225cebb20727cef0d288c6
SandervST <Sander.vansterkenburg@student.hu.nl>
2019-04-12 16:13:29 +0200

    Preprocessing done

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,8 +1,18 @@
 #include "StudentPreProcessing.h"
+#include "ImageFactory.h"
 
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	return nullptr;
+	auto intesityImage = ImageFactory::newIntensityImage(image.getWidth(), image.getHeight());
+
+	for (int w = 0; w < image.getWidth(); w++) {
+		for (int h = 0; h < image.getHeight(); h++) {
+			auto ColorPixel = image.getPixel(w, h);
+			Intensity i = (ColorPixel.r + ColorPixel.g + ColorPixel.b) / 3;
+			intesityImage->setPixel(w, h, i);
+		}
+	}
+	return intesityImage;
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
